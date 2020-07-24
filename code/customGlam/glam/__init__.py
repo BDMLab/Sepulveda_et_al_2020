ff7e298ec3ebe8de6b209438280ad657e6c27e32
Pradyumna <pradyumna@AHBM-PRADYUMNA.local>
2020-07-24 15:00:06 +0100

    updated datasets and code, first commit

---
+++ code/customGlam/glam/__init__.py
@@ -0,0 +1,11 @@
+from . import utils
+from . import components
+from . import simulation
+from . import driftSim
+from . import plots_pretty
+from . import plots_pretty_aDDM
+from . import plots_pretty_GLAM
+
+from .models import make_models, GLAM
+from .fit import fit_models
+from .plots import plot_fit
